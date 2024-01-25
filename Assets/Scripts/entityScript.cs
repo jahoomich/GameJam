12bd387ab8f011e446ffe05148a2568db432c114
jahoomich <janmichalak1998@gmail.com>
2024-01-25 12:46:07 +0000

    changed the way turns work

---
+++ Assets/Scripts/entityScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class entityScript : Entity
+{
+    public GameObject[] attackTargets;
+    public GameObject[] debuffTargets;
+    public string name;
+
+    private void Start()
+    {
+        health = maxhealth;
+    }
+
+    private void Update()
+    {
+        Debug.Log(name + "health is: " + health);
+    }
+
+    public void attackTarget()
+    {
+        attack(attackTargets, -5);
+    }
+
+    public void changeDebuff() {
+        Debuff(debuffTargets);
+    }
+}
--- Assets/gameMgr.cs
+++ Assets/gameMgr.cs
@@ -8,13 +8,13 @@ public class gameMgr : MonoBehaviour
 {
     //bigfatbum
 
-    public int memberIndex = 0;
-    public bool nextTurn = false;
-    public static bool wizardAction = false;
+    private int memberIndex = 0;
+    public bool nextTurnValid;
+    
 
-    bool canPerform = false;
+    public bool canPerform = false;
     //string array to store the party members
-    string[] partyMembers = { "wizard", "knight", "archer", "boss"};
+    string[] partyMembers = {"wizard", "archer", "knight", "boss"};
 
     //integer member for the action type
     public int actionType;
@@ -26,127 +26,78 @@ public class gameMgr : MonoBehaviour
     public GameObject actionBtn;
 
     //member for the cancel button game object
-    public GameObject cancelBtn;
+    public GameObject betrayBtn;
 
+    public GameObject[] characters;
+    public GameObject[] debuffCharacters;
     ///public GameObject x;
     //public GameObject y;
 
     private void Start()
     {
-        memberIndex = 0;
+        //memberIndex = 0;
         actionType = 0;
+        
     }
     private void Update()
     {
         //Debug.Log("Boss health is: " + x.GetComponent<Entity>().health);
         //Debug.Log("Knight health is: " + y.GetComponent<Entity>().health);
-        //Debug.Log("Index is: " + memberIndex);
+        Debug.Log("Index is: " + memberIndex);
         //Debug.Log("Active Member is: " + partyMembers[memberIndex]);
         //Debug.Log("Action type is: " + actionType);
-        //Debug.Log("Next turn value: " + nextTurn);
+        //Debug.Log("Next turn value: " + nextTurnValid);
 
-        switch (actionType) 
+        if (memberIndex == 0)
         {
-            case 0:
-                canPerform = false; break;
-            case 1:
-                canPerform = true; break;
-            case 2:
-                canPerform = true; break;
+            betrayBtn.SetActive(true);
         }
-
-        //responses to nextTurn values
-        switch (nextTurn) 
+        else 
         {
-            //if it is set to true -> I want to remove the cancel button
-            case true:
-                cancelBtn.SetActive(false);
-                setWizBtns(false);
-                break;
-            case false:
-                if (memberIndex == 0 && canPerform == false)
-                {
-                    cancelBtn.SetActive(false);
-                }
-                else {
-                    cancelBtn.SetActive(true);
-                    setWizBtns(true);
-                }
-                
-                break;
+            betrayBtn.SetActive(false);
         }
-
-        if (canPerform == true)
-        {
-            actionBtn.SetActive(true);
-            
-        }
-        else {
-            actionBtn.SetActive(false);
-            
-        }
-
-        //if the wizard is active
-        if (memberIndex == 0 && nextTurn == false)
-        {
-            setWizBtns(true);
-
-            /*
-            //display option to either attack or to perform traitor action
-            for (int i = 0; i < 2; i++)
-            {
-                wizardButtons[i].SetActive(true);
-
-            }
-            */
-        }
-        else
-        {
-            cancelBtn.SetActive(false);
-            actionType = 1;
-            //actionBtn.SetActive(true);
-            setWizBtns(false);
-        }
+      
     }
-
+    
     public void setAction(int x) 
     {
         actionType = x;
     }
 
-    public void checkTurn() { 
-        //set the next turn variable to true
-        nextTurn = true;
+    public void debuff()
+    {
+        characters[memberIndex].GetComponent<entityScript>().changeDebuff();
+        changeTurn();
     }
 
-    public void checkWizardTurn() {
-        if (memberIndex == 0) { 
-            wizardAction = true;
-        }
+    public void attack() {
+        
+        characters[memberIndex].GetComponent<entityScript>().attackTarget();
+        changeTurn();
+        
     }
+
+    public void increaseIndex() { 
+        //set the next turn variable to true
+        memberIndex ++;
+        actionType = 0;
+    }
+
+    
+
+    
 
     public void changeTurn() {
-        
-        
-
-        if (nextTurn == true || wizardAction == true)
+        if (memberIndex == 3)
         {
-
-            //check if member index has not reached the bound threshold
-            if (memberIndex == 3)
-            {
-                //reset back to 0
-                memberIndex = 0;
-                actionType = 0;
-            }
-            else
-            {
-                //increment the value to go to the next member
-                memberIndex++;
-            }
-            nextTurn = false;
-            wizardAction = false;
+            memberIndex = 0;
+                
         }
+        else
+        {
+            memberIndex++;
+        }
+        actionType = 0;
         
     }
 
