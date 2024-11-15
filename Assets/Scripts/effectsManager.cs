d1e969053f3021c3685d49c370ad7d3cbcbb1689
Lesya Pany <olypann@gmail.com>
2024-11-15 17:55:50 +0000

    added my changes

---
+++ Assets/Scripts/effectsManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effectsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //command to type out text + animation + color + sound effects
+
+    //command to screen shake
+
+    //command to zoom in on character
+
+    //command to call a healing/damage effect on characters
+
+    //command to call the game over animation + transition
+
+    //command to call the win game animation + transition
+
+
+
+}
